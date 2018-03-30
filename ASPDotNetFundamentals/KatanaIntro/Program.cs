d72ec9b80939d6dc3e198e315b12a7167f709c15
kabyab <bandokabs@gmail.com>
2018-03-30 14:54:17 +0530

    Setup server to run in IIS

--- ASPDotNetFundamentals/KatanaIntro/Program.cs
+++ ASPDotNetFundamentals/KatanaIntro/Program.cs
@@ -12,20 +12,20 @@ namespace KatanaIntro
     using System.Web.Http;
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string uri = "http://localhost:8080/";
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string uri = "http://localhost:8080/";
 
-            using (WebApp.Start<Startup>(uri))
-            {
-                Console.WriteLine("Started!");
-                Console.ReadKey();
-                Console.WriteLine("Stopped!!");
-            }
-        }
-    }
+    //        using (WebApp.Start<Startup>(uri))
+    //        {
+    //            Console.WriteLine("Started!");
+    //            Console.ReadKey();
+    //            Console.WriteLine("Stopped!!");
+    //        }
+    //    }
+    //}
 
     public class Startup
     {
